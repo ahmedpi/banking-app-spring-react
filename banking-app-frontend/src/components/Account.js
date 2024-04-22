d243a7c0aab662c40628e2eb76b0f826c406e2bb
ahmedpi <ahmedali.pisa@gmail.com>
2024-04-22 12:33:16 +0200

    input validation

--- banking-app-frontend/src/components/Account.js
+++ banking-app-frontend/src/components/Account.js
@@ -1,55 +1,92 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Container, Paper, Button } from '@mui/material';
 
 export default function Account() {
-    const paperStyle ={
-        padding: '50px 20px',
-        width:600,
-        margin:"20px auto"
+    const paperStyle = {
+        padding: '10px 20px',
+        width: 600,
+        margin: "20px auto"
     }
-    const [accountHolderName, setAccountHolderName] = useState('')
+    const [accountHolderName, setAccountHolderName] = useState(undefined)
     const [balance, setBalance] = useState(0)
-    
+    const [accounts, setAccounts] = useState([])
+    const [success, setSuccess] = useState(false)
+    const [successMessage,setSuccessMessage] = useState('')
+    const [errorMessage,setErrorMessage] = useState('')
+
+    const account = {
+        accountHolderName: accountHolderName,
+        balance: balance
+    }
+
     const handleSubmitClick = (e) => {
         e.preventDefault();
-        console.log(JSON.stringify({accountHolderName: accountHolderName, balance: balance}))
+        console.log(JSON.stringify(account))
+        setSuccess(false)
+        setSuccessMessage('')
+        setErrorMessage('')
 
-        fetch(
-            "http://localhost:8080/api/accounts",
-            {
-                method:"POST",
-                headers:{"Content-Type":"application/json"},
-                body:JSON.stringify({accountHolderName: accountHolderName, balance: balance})
-            }).then(() => {
-                console.log("New Account created")
-            })
+        if (e.target.checkValidity()) {
+            fetch(
+                "http://localhost:8080/api/accounts",
+                {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify(account)
+                }).then(() => {
+                    setSuccess(true)
+                    setSuccessMessage('New account created successfully!')
+                    console.log("New Account created")
+                })
+        } else {
+            setSuccess(false);
+            setErrorMessage("Form is invalid! Please check the required fields");
+        }
     }
 
-  return (
-    <Container>
-        <Paper elevator={3} style={paperStyle}>
-            <h1 style={{color: "blue"}}><u>Create Account</u></h1>
-    <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <TextField id="outlined-basic" label="Account Holder Name" variant="outlined" fullWidth value={accountHolderName} onChange={(e)=> setAccountHolderName(e.target.value)}/>
-      <TextField id="outlined-basic" label="Balance" variant="outlined" fullWidth 
-      value={balance} onChange={(e)=> setBalance(e.target.value)}/>
+    useEffect(() => {
+        fetch("http://localhost:8080/api/accounts")
+            .then(res => res.json())
+            .then((result) => {
+                setAccounts(result)
+            })
+    })
 
-    <Button variant="contained" onClick={handleSubmitClick}>Submit</Button>
+    return (
+        <Container>
+            <Paper elevator={3} style={paperStyle}>
+                <h1 style={{ color: "blue" }}><u>Create Account</u></h1>
+                <Box
+                    component="form" onSubmit={handleSubmitClick}
+                   // sx={{
+                   //     '& > :not(style)': { m: 1, width: '25ch' },
+                    //}}
+                    noValidate
+                    autoComplete="off"
+                >
+                    <TextField error id="outlined-basic" label="Account Holder Name" variant="outlined" required fullWidth value={accountHolderName} onChange={(e) => setAccountHolderName(e.target.value)} />
+                    <TextField error id="outlined-basic" label="Balance" variant="outlined" required  fullWidth margin="normal" 
+                        value={balance} onChange={(e) => setBalance(e.target.value)} />
+                        {success ? 
+                        <h4 style={{textAlign: "center", color:"green"}}>{successMessage}</h4>
+                        :  <h4 style={{textAlign: "center", color:"red"}}>{errorMessage}</h4> }
+                    <Button  variant="contained" type="submit">Submit</Button>
 
-    </Box>
-
-    {accountHolderName}
-    {balance}
-    </Paper>
-    </Container>
-  );
+                </Box>
+            </Paper>
+            <h1>Accounts</h1>
+            <Paper elevation={3} style={paperStyle}>
+                {accounts.map(account => (
+                    <Paper elevation={6} style={{ margin: "10px", padding: "15px", textAlign: "left" }} key={account.id}>
+                        Id: {account.id} <br />
+                        Name: {account.accountHolderName}  <br />
+                        Balance: {account.balance}
+                    </Paper>
+                ))
+                }
+            </Paper>
+        </Container>
+    );
 }
--- banking-app-frontend/src/components/Appbar.js
+++ banking-app-frontend/src/components/Appbar.js
@@ -7,24 +7,24 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Appbar() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Simple Banking Full Stack Application - Spring + React
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Simple Banking Full Stack Application - Spring + React
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
 }
